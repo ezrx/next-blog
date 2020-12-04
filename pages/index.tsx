6fb8cc35d639199dbef684e03fb8df9856694d4c
Reza Riyadhi <reza.riyadhi@gmail.com>
2020-12-05 03:25:00 +0700

    update homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,8 +8,6 @@ export default class Index extends Component {
         <div className="row">
           <div className="col-12">
             <h1>Home Page</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora molestiae assumenda iste debitis, deserunt nesciunt omnis odio quas ex et ab est sunt numquam ad sapiente impedit consequatur magni possimus.</p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod facere dolorem vel error modi rem nesciunt dicta. Tempora eius alias libero ipsum molestiae, suscipit eveniet quo, sed dignissimos velit ratione!</p>
           </div>
         </div>
       </Layout>
